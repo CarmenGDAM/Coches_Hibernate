46843c55961ecbf75976663d1f3c7db0149d6990
CarmenGDAM <carmen.garen@gmail.com>
2023-11-21 09:59:29 +0100

    commit creacion clase Coche y hibernateConf

---
+++ src/main/java/com/practica/coches_hibernate/model/Coche.java
@@ -0,0 +1,4 @@
+package com.practica.coches_hibernate.model;
+
+public class Coche {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.practica.coches_hibernate {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires org.hibernate.orm.core;
 
     opens com.practica.coches_hibernate to javafx.fxml;
     exports com.practica.coches_hibernate;
